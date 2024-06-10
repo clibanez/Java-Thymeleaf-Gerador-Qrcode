dfd87c870c3bded3b06c4859b05c18239aa4cdda
Clibanez <clibanezzcaldas@gmail.com>
2024-06-10 20:19:47 -0300

    Projeto inicial

---
+++ src/main/java/com/clibanez/geradorqrcode/GeradorqrcodeApplication.java
@@ -0,0 +1,13 @@
+package com.clibanez.geradorqrcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorqrcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorqrcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clibanez/geradorqrcode/GeradorqrcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clibanez.geradorqrcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorqrcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
